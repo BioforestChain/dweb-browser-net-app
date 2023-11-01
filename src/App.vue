4a92c666bdf9d41bc2d6028676fef967c9217769
wenhw <wen122056@gmail.com>
2023-11-01 14:58:09 +0800

    feat:plaoc依赖安装

--- src/App.vue
+++ src/App.vue
@@ -11,4 +11,9 @@ const key = computed(() => `${String(route.name || route.path)}-${new Date()}`)
 watchResize(() => {
   document.body.className = `platform-${isMobile() ? 'mobile' : 'desktop'}`
 })
+
+import NetProxy from '@/lib/ws'
+
+const ni = new NetProxy()
+ni.getNetInfo()
 </script>
